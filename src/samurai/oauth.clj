fcf90916ef01ae13b8927c644a41819b43972198
grafvetkindorff <vetkindorff@gmail.com>
2022-11-28 11:49:25 +0300

    cleanup ouath namespace

--- src/samurai/oauth.clj
+++ src/samurai/oauth.clj
@@ -4,7 +4,6 @@
            com.intuit.ipp.core.ServiceType
            com.intuit.ipp.data.Bill
            com.intuit.ipp.data.BillPayment
-           com.intuit.ipp.exception.FMSException
            com.intuit.ipp.security.IAuthorizer
            com.intuit.ipp.security.OAuth2Authorizer
            com.intuit.ipp.security.OAuthAuthorizer
@@ -29,31 +28,5 @@
         company-id (.getProperty prop "company.id")]
     (Context. oauth ServiceType/QBO company-id)) )
 
-
-;; String sql = "select * from account";
-;; QueryResult queryResult = service.executeQuery(sql);
-;; int count = queryResult.getEntities().size();
-
-;; BillPayment account = BillHelper.getBillPaymentFields(service);
-;; BillPayment savedBillPayment = service.add(account);
-
-(defn do-sql-query
-  [sql]
-  (let [service (DataService. (get-context))]
-    (try
-      (.executeQuery service sql)
-      (catch FMSException e
-        (map #(.getMessage %) (.getErrorList e))))))
-
-;; Get amounts
-(map (comp :totalAmt bean) (.getEntities (do-sql-query "select * from payment")))
-
-
-(defn get-bill-payment
-  []
-  (let [service (DataService. (get-context))]
-    (.findAll service (Bill.))))
-
-#_(get-bill-payment)
 
 (comment)
